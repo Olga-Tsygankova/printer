ec2732f75fde65bf1b3d30e4870963056a66e0ea
VLGKiwi <temav.i.p@mail.ru>
2024-09-02 09:24:06 +0300

    fix: fix error

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,8 +2,8 @@ import styles from './styles.module.css';
 
 export const MainPage = () => {
   return(
-    <>
+    <div className={styles.test}>
       компоненты принтера
-    </>
+    </div>
   )
 };
