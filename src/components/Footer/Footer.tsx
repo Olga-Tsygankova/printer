8cd83dc2fa5c037b85a42a2e86bd37cd2b31a78e
Olga Tsygankova <tsygankova@ispolcom.ru>
2024-09-03 22:23:52 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,55 @@
+import styles from './styles.module.css';
+import logo from './../../assets/svg/logo.svg';
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.logo}>
+        <img src={logo} alt=""/>
+      </div>
+      <div className={styles.ca}>
+        CA: 78sdf78sd78asd76asd87as67d65fd7s8
+      </div>
+      <div className={styles.links}>
+        <div className={styles.webLinks}>
+          <h4>
+            Web links
+          </h4>
+          <div className={styles.linksList}>
+            <a href="#">About the project</a>
+            <a href="#">How it works</a>
+            <a href="#">Roadmap</a>
+            <a href="#">Tokenomics</a>
+            <a href="#">Contacts and links</a>
+            <a href="#">Start Printing</a>
+          </div>
+        </div>
+        <div className={styles.socialLinks}>
+          <h4>
+            Social Links
+          </h4>
+          <div className={styles.linksList}>
+            <a href="#">Twitter</a>
+            <a href="#">Telegram</a>
+          </div>
+        </div>
+        <div className={styles.otherLinks}>
+          <h4>
+            Other Links
+          </h4>
+          <div className={styles.linksList}>
+            <a href="#">Etherscan</a>
+            <a href="#">Dextools</a>
+            <a href="#">Platform</a>
+            <a href="#">White paper</a>
+            <a href="#">Token</a>
+            <a href="#">Roadmap</a>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
+  );
+};
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.css';
 import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
 export const MainPage = () => {
   return(
     <div className={styles.main}>
       <Header/>
+      <Footer/>
     </div>
   )
 };
