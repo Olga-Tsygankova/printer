733151703105a44eea1ada24310456e892c37959
Olga Tsygankova <tsygankova@ispolcom.ru>
2024-09-05 00:40:36 +0300

    feat: add About

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,12 +2,14 @@ import styles from './styles.module.css';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { Banner } from '../../components/Banner';
+import { About } from '../../components/About/About.tsx';
 
 export const MainPage = () => {
   return(
     <div className={styles.main}>
       <Header/>
       <Banner/>
+      <About/>
       <Footer/>
     </div>
   )
