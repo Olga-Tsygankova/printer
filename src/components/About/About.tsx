8dd419b414c22735292b3813d070b9a135dc62b3
Olga Tsygankova <tsygankova@ispolcom.ru>
2024-09-12 01:15:44 +0300

    feat: привязка анимации к видимости блока

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,17 +1,45 @@
 import styles from './styles.module.css';
 import greenSpring from './../../assets/svg/greenSpring.svg';
 import { Windows } from './Windows/Windows.tsx';
+import { useEffect, useRef } from 'react';
 
 export const About = () => {
+  const textAboutRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (textAboutRef.current && entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+            textAboutRef.current.classList.add(styles.textAboutAnimated);
+          } else if (textAboutRef.current) {
+            textAboutRef.current.classList.remove(styles.textAboutAnimated);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (textAboutRef.current) {
+      observer.observe(textAboutRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
 
   return (
     <div id="about" className={styles.about}>
       <div className={styles.title}>
         <h2>
-          <span className={styles.textAbout}>About</span>
+          <span ref={textAboutRef} className={styles.textAbout}>About</span>
           &nbsp;the project
         </h2>
-        <img src={greenSpring} alt='' className={styles.greenSpring}/>
+        <img src={greenSpring} alt="" className={styles.greenSpring}/>
       </div>
       <Windows/>
     </div>
--- src/components/RoadMap/RoadMap.tsx
+++ src/components/RoadMap/RoadMap.tsx
@@ -1,12 +1,40 @@
 import styles from './styles.module.css';
 import { VerticalSteps } from './VerticalSteps';
+import { useEffect, useRef } from 'react';
 
 export const RoadMap = () => {
+  const textHowRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (textHowRef.current && entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+            textHowRef.current.classList.add(styles.textHowAnimated);
+          } else if (textHowRef.current) {
+            textHowRef.current.classList.remove(styles.textHowAnimated);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (textHowRef.current) {
+      observer.observe(textHowRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <div id='how' className={styles.roadmap}>
       <div className={styles.title}>
         <h2>
-          <span className={styles.textHow}>How</span>
+          <span ref={textHowRef} className={styles.textHow}>How</span>
           &nbsp;to create your token using Printer
         </h2>
       </div>
